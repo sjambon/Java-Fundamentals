359fb341a2a81220b9554af59687ea294801792d
SJ-RazerBlade <steven.jambon1@gmail.com>
2018-10-16 14:51:43 +0200

    Suicide

--- src/Labo4/Oefening8.java
+++ src/Labo4/Oefening8.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 /**
  * Dit programma controleert of het aan elkaar hechten van de cijfers in je nummerplaat deelbaar is door 4.
+ *
  * @author Steven Jambon
  * @version 15 oct 2018
  */
 
 public class Oefening8 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Geef je nummerplaat : ");
@@ -17,6 +19,6 @@ public class Oefening8 {
         String getal2 = nummerplaat.substring(6);
         getal2 = getal1 + getal2;
         int getal = Integer.parseInt(getal2);
-        System.out.println("Het getal " + getal2 + " is " + (getal%4==0 ? "" : "niet ") + "deelbaar door 4.");
+        System.out.println("Het getal " + getal2 + " is " + (getal % 4 == 0 ? "" : "niet ") + "deelbaar door 4.");
     }
 }
