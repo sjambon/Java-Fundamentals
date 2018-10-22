a561a567f8b087d271e571e70fd8b3f987ed94bb
SJ-RazerBlade <steven.jambon1@gmail.com>
2018-10-22 16:27:22 +0200

    Suicide

--- src/Labo5/Oefening9.java
+++ src/Labo5/Oefening9.java
@@ -7,11 +7,23 @@ public class Oefening9 {
         System.out.println("Geef de tijd:");
         Scanner scanner = new Scanner(System.in);
         String uur = scanner.next();
-        if (Character.isAlphabetic(uur.charAt(uur.length()-1))) {
-            char AP = uur.charAt(uur.length()-2);
-
-                //Omzetten naar normale tijd
+        if (Character.isAlphabetic(uur.charAt(uur.length() - 1))) { //Omzetten naar normale tijd
+            String ap = Character.toString(uur.charAt(uur.length() - 2));
+            int uurVal = Integer.parseInt(uur.substring(0, uur.length()-2));
+            if (ap.equals("A")) {
+                System.out.printf("Het is %s uur", uurVal);
+            } else {
+                System.out.printf("Het is %s uur", uurVal + 12);
             }
+        } else {
+            int uurVal = Integer.parseInt(uur);
+            if (uurVal > 12) {
+                uurVal -= 12;
+                uur = Integer.toString(uurVal) + "PM";
+            } else {
+                uur = Integer.toString(uurVal) + "AM";
+            }
+            System.out.printf("Het is %s", uur);
         }
     }
 }
