95690a6702bcbe78271d9c7025e96afdf2b74ff5
SJ-Desktop <steven.jambon@ugent.be>
2019-01-13 14:32:11 +0100

    BugFix: Labo 4: Oefening 7: VraagZin

--- src/Labo4/Oefening07.java
+++ src/Labo4/Oefening07.java
@@ -17,8 +17,14 @@ public class Oefening07 {
         int eersteSpace = zin.indexOf(" ");
         int tweedeSpace = zin.substring(eersteSpace + 1).indexOf(" ");
         String woord1 = zin.substring(0, eersteSpace);
-        String woord2 = zin.substring(eersteSpace + 1, eersteSpace + tweedeSpace + 1);
-        zin = zin.substring(eersteSpace + tweedeSpace + 1);
-        System.out.println(woord2 + " " + woord1 + zin + "?");
+        String woord2;
+        if (tweedeSpace == -1) {
+            woord2 = zin.substring(eersteSpace + 1);
+            System.out.println(woord2 + " " + woord1 + "?");
+        } else {
+            woord2 = zin.substring(eersteSpace + 1, eersteSpace + tweedeSpace + 1);
+            zin = zin.substring(eersteSpace + tweedeSpace + 1);
+            System.out.println(woord2 + " " + woord1 + zin + "?");
+        }
     }
 }
